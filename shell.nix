98e2ee5755bb41ef2eda0907013d2f17f3238d66
juanan-martin-lpz <juanan.martin.lpz@gmail.com>
2020-02-16 02:19:04 +0100

    Deleted fixup hook, not used. 0.9.1a-RC8

--- shell.nix
+++ shell.nix
@@ -19,21 +19,6 @@ let
 in pkgs.stdenv.mkDerivation {
   name = "starsector-shell";
 
-  fixupPhase = ''
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" jre_linux/bin/java 
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" jre_linux/bin/java_vm
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" jre_linux/bin/javaws
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" jre_linux/bin/jcontrol
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" jre_linux/bin/keytool
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" jre_linux/bin/orbd
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" jre_linux/bin/pack200
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" jre_linux/bin/policytool
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" jre_linux/bin/rmid
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" jre_linux/bin/rmiregistry
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" jre_linux/bin/servertool
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" jre_linux/bin/tnameserv
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" jre_linux/bin/unpack200
-  '';
 
 
   shellHook = ''
